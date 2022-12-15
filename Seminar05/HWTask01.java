a62f68465e1d990de0f6ef6a353b8c746e5b0822
HenkSeed <adaner@inbox.ru>
2022-12-16 00:24:28 +0700

    Added the Seminar05 HWTask03 decision and modified HWTask01 using TreeMap

--- Seminar05/HWTask01.java
+++ Seminar05/HWTask01.java
@@ -1,36 +1,41 @@
 package Seminar05;
 
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class HWTask01 {
    public static void main(String[] args) {
-      Map<String, String> tf = new HashMap<>();
-      Map<String, String> linkedHashMap = new LinkedHashMap<>();
-      String nameToFind = "Иванова";
-      tf.put("+7(286)5362773", "Иванова");
-      tf.put("+7(348)8940448", "Петров");
-      tf.put("+7(603)1322915", "Конюхов");
-      tf.put("+7(240)2615321", "Иванова");
-      tf.put("+7(124)2941317", "Ложечкин");
-      tf.put("+7(605)1448688", "Иванов");
-      tf.put("+7(666)9677425", "Петров");
+      Map<String, String> phonesMap = new HashMap<>();
+      TreeMap<String, ArrayList<String>> treeMap = new TreeMap<>();
 
-      // Prints phonebook to terminal
-      for (var client : tf.entrySet()) {
+      String nameToFind = "Иванова";
+      phonesMap.put("+7(286)5362773", "Иванова");
+      phonesMap.put("+7(348)8940448", "Петров");
+      phonesMap.put("+7(603)1322915", "Конюхов");
+      phonesMap.put("+7(240)2615321", "Иванова");
+      phonesMap.put("+7(124)2941317", "Ложечкин");
+      phonesMap.put("+7(605)1448688", "Иванов");
+      phonesMap.put("+7(666)9677425", "Петров");
+
+      // Printing phonebook to the terminal
+      for (var client : phonesMap.entrySet()) {
          System.out.printf("[%s: %s]\n", client.getKey(), client.getValue());
       }
       System.out.println();
 
-      // Gets entries which are the equals to the nameToFind - variable
-      for (Map.Entry<String, String> clientsName : tf.entrySet()) {
+      // Creating the list of the phones of the person to find
+      ArrayList<String> phonesList = new ArrayList<>();
+
+      // Getting entries which are the equals to the nameToFind - variable
+      for (Map.Entry<String, String> clientsName : phonesMap.entrySet()) {
          if (clientsName.getValue().equals(nameToFind)) {
-            linkedHashMap.put(clientsName.getKey(), clientsName.getValue());
+            phonesList.add(clientsName.getKey());
          }
       }
-      // Prints chosen entries
-      System.out.println(linkedHashMap);
+      treeMap.put(nameToFind, phonesList);
+      System.out.println(treeMap.entrySet());
    }
 }
 
--- Seminar05/HWTask03.java
+++ Seminar05/HWTask03.java
@@ -1,8 +1,71 @@
 package Seminar05;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class HWTask03 {
    public static void main(String[] args) {
-      System.out.println("Hi !");
+      Scanner sc = new Scanner(System.in);
+      System.out.printf("Введите число в римском формате:\n");
+      String romeString = sc.nextLine();
+      sc.close();
+      char[] romeDigits = new char[romeString.length()];
+      romeDigits = romeString.toCharArray();
+      int pointer = 0;
+      // Map<String, Integer> romeArabMap = new LinkedHashMap<>();
+      Map<Character, Integer> romeArabMap = new HashMap<>();
+      romeArabMap.put('I', 1);
+      romeArabMap.put('V', 5);
+      romeArabMap.put('X', 10);
+      romeArabMap.put('L', 50);
+      romeArabMap.put('C', 100);
+      romeArabMap.put('D', 500);
+      romeArabMap.put('M', 1000);
+
+      int decimalNumber = romeArabMap.get(romeDigits[pointer]);
+
+      if (!forbiddenMix(romeDigits)) {
+         do {
+            // If the next digit is greater than the specified
+            if (romeArabMap.get(romeDigits[pointer]) >= romeArabMap.get(romeDigits[pointer + 1])) {
+               decimalNumber = decimalNumber + romeArabMap.get(romeDigits[pointer + 1]);
+               pointer++;
+               // Else if the next digit is less than the specified
+            } else if (romeArabMap.get(romeDigits[pointer]) < romeArabMap.get(romeDigits[pointer + 1])) {
+               decimalNumber = decimalNumber + romeArabMap.get(romeDigits[pointer + 1])
+                     - 2 * romeArabMap.get(romeDigits[pointer]);
+               pointer++;
+            }
+
+         } while (pointer < romeDigits.length - 1);
+
+         System.out.printf("Десятичное = %d", decimalNumber);
+      }
+   }
+
+   static boolean forbiddenMix(char[] ch) {
+      // Checks if there are more then 3 the same digits
+      if (ch.length > 3) {
+         for (int i = 0; i < ch.length - 3; i++) {
+            if (ch[i] == ch[i + 1] && ch[i] == ch[i + 2] && ch[i] == ch[i + 3]) {
+               System.out.println("Слишком много одинаковых цифр");
+               return true;
+            }
+         }
+      }
+      // Checks if there are duble V, L or D
+      if (ch.length > 1) {
+         for (int i = 0; i < ch.length - 1; i++) {
+            if (((ch[i] == 'V') && (ch[i + 1] == 'V'))
+                  || ((ch[i] == 'L') && (ch[i + 1] == 'L'))
+                  || ((ch[i] == 'D') && (ch[i + 1] == 'D'))) {
+               System.out.println("Дубляж V, L или D недопустим");
+               return true;
+            }
+         }
+      }
+      return false;
    }
 }
 
@@ -10,4 +73,12 @@ public class HWTask03 {
  * Написать метод, который переведет число из римского формата записи в
  * арабский. Например, MMXXII = 2022 (правила см. во вложениях к уроку, где
  * ссылка на репозиторий).
+ */
+/*
+ * MCMLXXXIV = 1984
+ * MMXXII = 2022
+ * XVII = 17
+ * XXIX = 29
+ * MCCCLXXIV = 1374
+ * MCDLXXIV = 1474
  */
